090ef3966a3fb7e478908ab2a28f3c58c0e72cd8
tukumo-n9 <tukumon9.work@gmail.com>
2023-11-23 22:01:58 +0900

    問題画面と結果画面の遷移を作る

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const [isResult, setIsResult] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <header className='header'>
+          <h1>ペルソナシリーズおすすめ作品診断</h1>
+        </header>
+        <div className="container">
+          {isResult
+            ? <Result retryClick={setIsResult} />
+            : <Question answerClick={setIsResult} />
+          }
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function Question({ answerClick }) {
+  return (
+    <>
+      <div className="question">
+        <p>Q1. ペルソナシリーズを知っていますか？</p>
+      </div>
+      <div className="answer">
+        <button onClick={() => answerClick(true)}>はい</button>
+        <button onClick={() => answerClick(true)} >いいえ</button>
+      </div>
+    </>
+  );
+}
+
+function Result({ retryClick }) {
+  return (
+    <>
+      <div className="result">
+        <p>あなたにおすすめの作品は<em>ペルソナ3</em>です！</p>
+      </div>
+      <div className="description">
+        <p>ペルソナ3の説明</p>
+      </div>
+      <div className="retry">
+        <button onClick={() => retryClick(false)}>もう一度診断する</button>
+      </div>
+    </>
+  )
+}
