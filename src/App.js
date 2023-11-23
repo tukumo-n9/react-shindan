31263f18d37c992c06b714df020d145e231fac7c
tukumo-n9 <tukumon9.work@gmail.com>
2023-11-23 22:52:25 +0900

    各質問のテキストと加点対象をオブジェクトのstateで表現する

--- src/App.js
+++ src/App.js
@@ -8,10 +8,58 @@ export default function App() {
     p4: 0,
     p5: 0,
   });
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const questionData = [
+    {
+      question: 'Q1. ペルソナ3が好きですか？',
+      yes: 'p3',
+      no: '',
+    },
+    {
+      question: 'Q2. ペルソナ4が好きですか？',
+      yes: 'p4',
+      no: '',
+    },
+    {
+      question: 'Q3. ペルソナ5が好きですか？',
+      yes: 'p5',
+      no: '',
+    },
+  ];
 
-  const answer = () => {
-    setPoint({...point, p3: point.p3 + 1});
-    setIsResult(true);
+  const answer = (target) => {
+    switch (target) {
+      case 'p3':
+        setPoint({...point, p3: point.p3 + 1});
+        break;
+      case 'p4':
+        setPoint({...point, p4: point.p4 + 1});
+        break;
+      case 'p5':
+        setPoint({...point, p5: point.p5 + 1});
+        break;
+      default:
+        break;
+    }
+    if (currentQuestion === questionData.length - 1) {
+      setIsResult(true);
+    } else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  };
+
+  const calclateResult = () => {
+    const max = Math.max(point.p3, point.p4, point.p5);
+    switch (max) {
+      case point.p3:
+        return 'ペルソナ3';
+      case point.p4:
+        return 'ペルソナ4';
+      case point.p5:
+        return 'ペルソナ5';
+      default:
+        return 'エラー：結果が正しく計算できませんでした。';
+    }
   }
 
   const retry = () => {
@@ -21,6 +69,7 @@ export default function App() {
       p4: 0,
       p5: 0,
     });
+    setCurrentQuestion(0);
   };
 
   return (
@@ -32,7 +81,7 @@ export default function App() {
         <div className="container">
           {isResult
             ? <Result retryClick={retry} />
-            : <Question answerClick={answer} />
+            : <Question currentQuestionData={questionData[currentQuestion]} answerClick={answer} />
           }
           <div className="point-viewer">
             <h2>各選択肢のポイント</h2>
@@ -46,15 +95,15 @@ export default function App() {
   );
 }
 
-function Question({ answerClick }) {
+function Question({ currentQuestionData, answerClick }) {
   return (
     <>
       <div className="question">
-        <p>Q1. ペルソナ3が好きですか？</p>
+        <p>{currentQuestionData.question}</p>
       </div>
       <div className="answer">
-        <button onClick={answerClick}>はい</button>
-        <button onClick={answerClick} >いいえ</button>
+        <button onClick={() => answerClick(currentQuestionData.yes)}>はい</button>
+        <button onClick={() => answerClick(currentQuestionData.no)} >いいえ</button>
       </div>
     </>
   );
